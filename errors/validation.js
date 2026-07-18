39df32501d47312e37350682f949e8feefe4da38
Ankit Chaudhary <Ankitchy158@gmail.com>
2026-07-18 11:13:42 +0545

    added errors and validation

---
+++ errors/validation.js
@@ -0,0 +1,9 @@
+export class ValidationError extends Error {
+  constructor(message, errors = []) {
+    super(message);
+
+    this.name = "ValidationError";
+    this.status = 400;
+    this.errors = errors;
+  }
+}
